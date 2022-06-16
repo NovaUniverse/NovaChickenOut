5e22efae89e5b9f66ebb2528e5dc0c2316165e6a
BrunoGamerGH <104372237+BrunoGamerGH@users.noreply.github.com>
2022-06-16 20:41:39 -0300

    this only exists because im lazy to add int level

---
+++ src/net/novauniverse/games/chickenout/game/mobs/Enchant.java
@@ -0,0 +1,23 @@
+package net.novauniverse.games.chickenout.game.mobs;
+
+import org.bukkit.enchantments.Enchantment;
+
+public class Enchant {
+
+    Enchantment enchantment;
+    int level = 0;
+    public Enchant(Enchantment enchantment, int level) {
+
+        this.enchantment = enchantment;
+        this.level = level;
+
+    }
+
+    public Enchantment getEnchantment() {
+        return enchantment;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
