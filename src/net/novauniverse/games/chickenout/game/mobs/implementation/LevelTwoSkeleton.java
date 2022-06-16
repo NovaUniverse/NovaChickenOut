30c832bf0a2a555b621d59eafe6f56aa463f89e7
BrunoGamerGH <104372237+BrunoGamerGH@users.noreply.github.com>
2022-06-16 20:49:36 -0300

    random armor skeleton
    thats it

--- src/net/novauniverse/games/chickenout/game/mobs/implementation/LevelTwoSkeleton.java
+++ src/net/novauniverse/games/chickenout/game/mobs/implementation/LevelTwoSkeleton.java
@@ -1,31 +1,40 @@
 package net.novauniverse.games.chickenout.game.mobs.implementation;
 
-import net.novauniverse.games.chickenout.game.mobs.ChickenOutMobProvider;
-import net.zeeraa.novacore.spigot.utils.ItemBuilder;
+import net.novauniverse.games.chickenout.game.mobs.ArmorMaterialType;
+import net.novauniverse.games.chickenout.game.mobs.MobProviderWithRandomArmor;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-public class LevelTwoSkeleton implements ChickenOutMobProvider {
+public class LevelTwoSkeleton extends MobProviderWithRandomArmor {
     @Override
     public int getLevel() {
         return 2;
     }
 
+
+
+
+
     @Override
     public Creature spawn(Location location, Player player) {
+
         Creature creature = (Creature) location.getWorld().spawnEntity(location, EntityType.SKELETON);
 
         creature.getEquipment().clear();
 
         creature.setCustomName("Skeleton");
         creature.setCustomNameVisible(true);
-        creature.getEquipment().setHelmet(new ItemBuilder(Material.LEATHER_HELMET).setAmount(1).setUnbreakable(true).build());
-        creature.getEquipment().setChestplate(new ItemBuilder(Material.LEATHER_CHESTPLATE).setAmount(1).setUnbreakable(true).build());
-        creature.getEquipment().setItemInMainHand(new ItemBuilder(Material.BOW).setAmount(1).setUnbreakable(true).build());
+
+        setTimesYouCanRandomize(2);
+        armorRandomizer(creature, ArmorMaterialType.CHAINMAIL);
+        creature.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE,0,false,false,false));
+
         return creature;
     }
+
+
 }
