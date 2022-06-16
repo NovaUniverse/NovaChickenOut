01960294e0bd77f29b9ce2d5532f5f148cb37c46
BrunoGamerGH <104372237+BrunoGamerGH@users.noreply.github.com>
2022-06-16 19:28:56 -0300

    holy shit bruno actually did something

---
+++ src/net/novauniverse/games/chickenout/game/mobs/implementation/LevelTwoSkeleton.java
@@ -0,0 +1,31 @@
+package net.novauniverse.games.chickenout.game.mobs.implementation;
+
+import net.novauniverse.games.chickenout.game.mobs.ChickenOutMobProvider;
+import net.zeeraa.novacore.spigot.utils.ItemBuilder;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class LevelTwoSkeleton implements ChickenOutMobProvider {
+    @Override
+    public int getLevel() {
+        return 2;
+    }
+
+    @Override
+    public Creature spawn(Location location, Player player) {
+        Creature creature = (Creature) location.getWorld().spawnEntity(location, EntityType.SKELETON);
+
+        creature.getEquipment().clear();
+
+        creature.setCustomName("Skeleton");
+        creature.setCustomNameVisible(true);
+        ((Ageable) creature).setAdult();
+        creature.getEquipment().setHelmet(new ItemBuilder(Material.LEATHER_HELMET).setAmount(1).setUnbreakable(true).build());
+        creature.getEquipment().setChestplate(new ItemBuilder(Material.LEATHER_CHESTPLATE).setAmount(1).setUnbreakable(true).build());
+        return creature;
+    }
+}
