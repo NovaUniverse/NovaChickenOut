a8144a05d20f10f0f019b66fc7c550a95983ad5b
BrunoGamerGH <104372237+BrunoGamerGH@users.noreply.github.com>
2022-06-16 21:09:19 -0300

    strongest scratch enjoyer

---
+++ src/net/novauniverse/games/chickenout/game/mobs/implementation/level1/LevelOneBabyZombie.java
@@ -0,0 +1,26 @@
+package net.novauniverse.games.chickenout.game.mobs.implementation.level1;
+
+import net.novauniverse.games.chickenout.game.mobs.ChickenOutMobProvider;
+import org.bukkit.Location;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class LevelOneBabyZombie implements ChickenOutMobProvider {
+
+    @Override
+    public int getLevel() {
+        return 1;
+    }
+
+    @Override
+    public Creature spawn(Location location, Player player) {
+        Creature creature = (Creature) location.getWorld().spawnEntity(location, EntityType.ZOMBIE);
+        creature.getEquipment().clear();
+        creature.setCustomName("Baby Zombie");
+        creature.setCustomNameVisible(true);
+        ((Ageable) creature).setBaby();
+        return creature;
+    }
+}
