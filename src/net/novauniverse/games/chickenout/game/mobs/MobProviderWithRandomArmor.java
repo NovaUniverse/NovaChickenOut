55b3151f3e7c18d809bf78a27b7a766aab786ab4
Anton udén <udenanton@hotmail.com>
2022-06-17 02:21:22 +0200

    code formatting

--- src/net/novauniverse/games/chickenout/game/mobs/MobProviderWithRandomArmor.java
+++ src/net/novauniverse/games/chickenout/game/mobs/MobProviderWithRandomArmor.java
@@ -5,88 +5,91 @@ import net.zeeraa.novacore.spigot.utils.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.entity.Creature;
 
-
 import java.util.ArrayList;
 
 public abstract class MobProviderWithRandomArmor implements ChickenOutMobProvider {
 
-    boolean hasHelmet = false;
-    boolean hasChestplate = false;
-    boolean hasLeggings = false;
-    boolean hasBoots = false;
-    int timesYouCanRandomize = 0;
-    int timesRandomized = 0;
-    public void setTimesYouCanRandomize(int timesYouCanRandomize) {
-        this.timesYouCanRandomize = timesYouCanRandomize;
-    }
-    public int getTimesYouCanRandomize() {
-        return timesYouCanRandomize;
-    }
-    public int getTimesRandomized() {
-        return timesRandomized;
-    }
-    public void setTimesRandomized(int timesRandomized) {
-        this.timesRandomized = timesRandomized;
-    }
-    public void armorRandomizer(Creature creature, ArmorMaterialType type) {
-        armorRandomizer(creature,type, new ArrayList<Enchant>());
-    }
-    public void armorRandomizer(Creature creature, ArmorMaterialType type, ArrayList<Enchant> enchantments) {
-        int random = RandomGenerator.generate(1,4);
-        if (getTimesRandomized() == getTimesYouCanRandomize())
-             return;
+	boolean hasHelmet = false;
+	boolean hasChestplate = false;
+	boolean hasLeggings = false;
+	boolean hasBoots = false;
+	int timesYouCanRandomize = 0;
+	int timesRandomized = 0;
 
-        if (random == 1) {
-            if (!hasHelmet) {
-                ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_HELMET")).setUnbreakable(true);
+	public void setTimesYouCanRandomize(int timesYouCanRandomize) {
+		this.timesYouCanRandomize = timesYouCanRandomize;
+	}
 
-                if (!(enchantments.isEmpty() || enchantments == null))
-                    enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(),enchant.getLevel(),false)));
+	public int getTimesYouCanRandomize() {
+		return timesYouCanRandomize;
+	}
 
-                creature.getEquipment().setHelmet(item.build());
-                setTimesRandomized(getTimesRandomized() + 1);
-                hasBoots = true;
+	public int getTimesRandomized() {
+		return timesRandomized;
+	}
 
+	public void setTimesRandomized(int timesRandomized) {
+		this.timesRandomized = timesRandomized;
+	}
 
+	public void armorRandomizer(Creature creature, ArmorMaterialType type) {
+		armorRandomizer(creature, type, new ArrayList<Enchant>());
+	}
 
-            }
-        } else if (random == 2) {
-            if (!hasChestplate) {
-                ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_CHESTPLATE")).setUnbreakable(true);
+	public void armorRandomizer(Creature creature, ArmorMaterialType type, ArrayList<Enchant> enchantments) {
+		int random = RandomGenerator.generate(1, 4);
+		if (getTimesRandomized() == getTimesYouCanRandomize())
+			return;
 
-                if (!(enchantments.isEmpty() || enchantments == null))
-                    enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(),enchant.getLevel(),false)));
+		if (random == 1) {
+			if (!hasHelmet) {
+				ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_HELMET")).setUnbreakable(true);
 
-                creature.getEquipment().setHelmet(item.build());
-                setTimesRandomized(getTimesRandomized() + 1);
-                hasChestplate = true;
+				if (!(enchantments.isEmpty() || enchantments == null))
+					enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(), enchant.getLevel(), false)));
 
-            }
-        }else if (random == 3) {
-            if (!hasLeggings) {
-                ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_LEGGINGS")).setUnbreakable(true);
+				creature.getEquipment().setHelmet(item.build());
+				setTimesRandomized(getTimesRandomized() + 1);
+				hasBoots = true;
 
-                if (!(enchantments.isEmpty() || enchantments == null))
-                    enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(),enchant.getLevel(),false)));
+			}
+		} else if (random == 2) {
+			if (!hasChestplate) {
+				ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_CHESTPLATE")).setUnbreakable(true);
 
-                creature.getEquipment().setHelmet(item.build());
-                setTimesRandomized(getTimesRandomized() + 1);
-                hasLeggings = true;
+				if (!(enchantments.isEmpty() || enchantments == null))
+					enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(), enchant.getLevel(), false)));
 
-            }
-        } else if (random == 4) {
-            if (!hasBoots) {
-                ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_BOOTS")).setUnbreakable(true);
+				creature.getEquipment().setHelmet(item.build());
+				setTimesRandomized(getTimesRandomized() + 1);
+				hasChestplate = true;
 
-                if (!(enchantments.isEmpty() || enchantments == null))
-                    enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(),enchant.getLevel(),false)));
+			}
+		} else if (random == 3) {
+			if (!hasLeggings) {
+				ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_LEGGINGS")).setUnbreakable(true);
 
-                creature.getEquipment().setHelmet(item.build());
-                setTimesRandomized(getTimesRandomized() + 1);
-                hasBoots = true;
+				if (!(enchantments.isEmpty() || enchantments == null))
+					enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(), enchant.getLevel(), false)));
 
-            }
-        }
-        armorRandomizer(creature, type);
-    }
+				creature.getEquipment().setHelmet(item.build());
+				setTimesRandomized(getTimesRandomized() + 1);
+				hasLeggings = true;
+
+			}
+		} else if (random == 4) {
+			if (!hasBoots) {
+				ItemBuilder item = new ItemBuilder(Material.valueOf(type + "_BOOTS")).setUnbreakable(true);
+
+				if (!(enchantments.isEmpty() || enchantments == null))
+					enchantments.forEach((enchant -> item.addEnchant(enchant.getEnchantment(), enchant.getLevel(), false)));
+
+				creature.getEquipment().setHelmet(item.build());
+				setTimesRandomized(getTimesRandomized() + 1);
+				hasBoots = true;
+
+			}
+		}
+		armorRandomizer(creature, type);
+	}
 }
