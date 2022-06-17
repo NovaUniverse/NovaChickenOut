afce54297cb94d9556a798ae2c77dcde8efd8d4e
BrunoGamerGH <104372237+BrunoGamerGH@users.noreply.github.com>
2022-06-16 21:08:42 -0300

    why is it so big tho

---
+++ src/net/novauniverse/games/chickenout/game/mobs/implementation/level1/LevelOneSpider.java
@@ -0,0 +1,24 @@
+package net.novauniverse.games.chickenout.game.mobs.implementation.level1;
+
+import net.novauniverse.games.chickenout.game.mobs.ChickenOutMobProvider;
+import org.bukkit.Location;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class LevelOneSpider implements ChickenOutMobProvider {
+
+    @Override
+    public int getLevel() {
+        return 1;
+    }
+
+    @Override
+    public Creature spawn(Location location, Player player) {
+        Creature creature = (Creature) location.getWorld().spawnEntity(location, EntityType.SPIDER);
+        creature.getEquipment().clear();
+        creature.setCustomName("Spider");
+        creature.setCustomNameVisible(true);
+        return creature;
+    }
+}
