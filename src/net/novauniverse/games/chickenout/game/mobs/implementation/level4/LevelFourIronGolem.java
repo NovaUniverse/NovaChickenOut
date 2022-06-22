70d42c54eb3ebc7f8129e8190c0668dc8d726f45
Anton udén <udenanton@hotmail.com>
2022-06-23 00:58:58 +0200

    aaaaaaaaaaaaaaaaaaaaaaa

--- src/net/novauniverse/games/chickenout/game/mobs/implementation/level4/LevelFourIronGolem.java
+++ src/net/novauniverse/games/chickenout/game/mobs/implementation/level4/LevelFourIronGolem.java
@@ -5,27 +5,25 @@ import org.bukkit.Location;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class LevelFourIronGolem implements ChickenOutMobProvider {
-    @Override
-    public int getLevel() {
-        return 4;
-    }
+	@Override
+	public int getLevel() {
+		return 4;
+	}
 
-    @Override
-    public Creature spawn(Location location, Player player) {
-        Creature creature = (Creature) location.getWorld().spawnEntity(location, EntityType.IRON_GOLEM);
+	@Override
+	public Creature spawn(Location location, Player player) {
+		Creature creature = (Creature) location.getWorld().spawnEntity(location, EntityType.IRON_GOLEM);
 
-        creature.getEquipment().clear();
+		creature.getEquipment().clear();
 
-        creature.setCustomName("Iron Golem");
-        creature.setCustomNameVisible(true);
+		creature.setCustomName("Iron Golem");
+		creature.setCustomNameVisible(true);
 
+		// creature.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,
+		// Integer.MAX_VALUE,1,false,false));
 
-        creature.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE,1,false,false));
-
-        return creature;
-    }
+		return creature;
+	}
 }
